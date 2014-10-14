9774e3b63b96ae271cd5dc15c5d12e2dcd7dab5a
larsland <lars_la@live.com>
2014-10-14 15:17:03 +0200

    board

--- SA.py
+++ SA.py
@@ -1,12 +1,18 @@
 __author__ = 'trulsmp'
 import math
 
+#m = int(input("How many rows? "))
+#n = int(input("How many cols? "))
+#k = int(input("How many eggs? "))
+m = n = 5
+k = 2
 
-class Board():
+class Board:
     BoardArray = [[]]
-    def __init__(self):
-        pass
-
+    def __init__(self, m, n, k):
+        self.rows = m
+        self.cols = n
+        self.eggs = k
 
     def validate_board(self):
         pass
@@ -15,10 +21,12 @@ class Board():
         pass
 
     def initiate_board(self):
-        pass
-
-
-
+        board = []
+        for i in range(0,m):
+            board.append([])
+            for j in range(0,n):
+                board[i].append('X')
+        return board
 
 
 
@@ -26,8 +34,10 @@ class Board():
 def SA():
     target = 10  # ?????
     temperature = 20  # ???
-    board = Board()
+    board = Board(m, n, k)
     board.initiate_board()
+    start = board[0][0]
+
     F = objective(board)  # Objective function
     while True:
         if F > target:
@@ -55,15 +65,11 @@ def SA():
 def Generate_neighbours():
     return 0
 
-
-
 def objective(P):
     return 0
 
-
 def evaluate_neighbours():
     pass
-
 
 def generate_neighbours():
     pass
