83514032068f4e54f8606cdf913e1762581c8a28
larsland <lars_la@live.com>
2014-10-14 16:09:32 +0200

    added generate first method

--- SA.py
+++ SA.py
@@ -1,5 +1,6 @@
 __author__ = 'trulsmp'
 import math
+import random
 
 #m = int(input("How many rows? "))
 #n = int(input("How many cols? "))
@@ -7,42 +8,30 @@ import math
 m = n = 5
 k = 2
 
-class Board:
-    board_array = []
-    def __init__(self, m, n, k):
-        self.rows = m
-        self.cols = n
-        self.eggs = k
-        board_array = self.initiate_board()
-
-    def validate_board(self):
-        return True
-
-    def calculate_score(self):
-        score = 0
-        for i in range(0,m):
-            for j in range(0,n):
-                if [i][j] == 1:
-                    score += 1
 
 
-    def initiate_board(self):
-        board = []
-        for i in range(0,m):
-            board.append([])
-            for j in range(0,n):
-                self.board_array[i].append(0)
-        return board
+def validate_board(self):
+    return True
 
-    def generate_start(self):
+def calculate_score(self):
+    score = 0
+    for i in range(0,m):
+        for j in range(0,n):
+            if [i][j] == 1:
+                score += 1
 
-        pass
 
-    def print_board(self):
-        for i in range(0,m):
-            for j in range(0,n):
-                print(self.board_array[i][j])
-            print("\n")
+
+
+
+
+
+
+def print_board(self):
+    for i in range(0,m):
+        for j in range(0,n):
+            print(self.board_array[i][j])
+        print("\n")
 
 
 
@@ -55,8 +44,9 @@ def SA():
 
 
     temperature = 3000  # ???
-    board = Board(m, n, k)
-    board.generateStart()
+    board = initiate_board(m, n)
+    print (generate_start(board))
+
 
     F = objective(board)  # Objective function
 
@@ -79,7 +69,25 @@ def SA():
 
         temperature = temperature - 1 '''''
 
+def initiate_board(m, n):
+    board = []
+    for i in range(0,m):
+        board.append([])
+        for j in range(0,n):
+            board[i].append('0')
+    return board
 
+def generate_start(board):
+    eggs = 10
+    while True:
+        randomRow = random.randint(0, m - 1)
+        randomCol = random.randint(0, n - 1)
+        if board[randomRow][randomCol] == '0':
+            board[randomRow][randomCol] = '1'
+            eggs -= 1
+        if eggs == 0:
+            break
+    print(board)
 
 def objective(board):
     if board.validate_board():
